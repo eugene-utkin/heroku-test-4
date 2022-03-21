a17a7c9b9ba468a07d99eeab59e4dec9e8dda72f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-21 22:12:47 +0500

    ready for deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  root 'chatroom#index'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  post 'message', to: 'messages#create'
-
-  mount ActionCable.server, at: '/cable'
+  root 'welcome#index'
 end
