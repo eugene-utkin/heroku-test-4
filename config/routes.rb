dff5a5c31aabb27e382973f95f1536692fe660fc
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-04-28 22:02:36 +0500

    bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
-  resources :users, only: [:show]
   devise_for :users
+  resources :users, only: [:show]
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
